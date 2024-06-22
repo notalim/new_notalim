5874d3426df5d57d575992157c952b7df1e15e0c
notalim <alimneedstowork@gmail.com>
2024-06-22 13:13:44 -0400

    added resume section; dk why ddidn't do it sooner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,17 @@ import Typewriter from "./Typewriter";
 import phrases from "../assets/phrases";
 
 const Navbar = () => {
-
     const scrollToChangelog = () => {
         const changelogSection = document.getElementById("changelogSection");
         if (changelogSection) {
             // scroll a bit more above the changelog section
-            changelogSection.scrollIntoView({ behavior: "smooth", block: "start" });
+            changelogSection.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
         }
     };
-    
+
     return (
         <div className="sticky top-0 z-10 ">
             <nav className="flex justify-between items-center p-4 bg-black text-white border-b border-white">
@@ -19,7 +21,17 @@ const Navbar = () => {
                     <img src="/favicon.ico" alt="notalim" className="w-6 h-6" />
                     <div className="text-md font-light">notalim</div>
                 </div>
+
                 <ul className="flex space-x-4">
+                    <a
+                        href="/notalim_resume.pdf"
+                        target="_blank"
+                        download="Resume_Alim_Kassymov.pdf"
+                    >
+                        <li className="cursor-pointer uppercase text-sm font-light">
+                            Resume
+                        </li>
+                    </a>
                     <a href="https://github.com/notalim">
                         <li className="cursor-pointer uppercase text-sm font-light">
                             Github
