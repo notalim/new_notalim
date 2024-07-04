681623d5909561aca13931daf601960a3edd6afd
notalim <alimneedstowork@gmail.com>
2024-07-04 17:08:54 -0400

    added support for coauthors in the changelog

--- src/components/ChangelogItem.jsx
+++ src/components/ChangelogItem.jsx
@@ -5,15 +5,46 @@ const parseDate = (date) => {
     return dateObj.toISOString().split("T")[0];
 };
 
-const parseMessage = (message, length = 30) => {
+const parseMessage = (message, length = 200) => {
     if (!message) return "";
     return message.length > length
         ? message.substring(0, length) + "..."
         : message;
 };
 
+const parseCoAuthors = (message) => {
+    const coAuthors = [];
+    const lines = message.split("\n");
+    const coAuthorRegex =
+        /Co-authored-by: [^<]+<(\w+)@users.noreply.github.com>/;
+
+    lines.forEach((line) => {
+        const match = line.match(coAuthorRegex);
+        if (match) {
+            coAuthors.push(match[1]); // match[1] is the captured username
+        }
+    });
+    return coAuthors;
+};
+
+const displayAuthors = (authors) => {
+    
+    return authors.map((author, index) => (
+        <div key={index}>
+            <span className="ml-1 text-xxs bg-black text-white">{author}</span>
+            {index < authors.length - 1 && (
+                <span className="text-xs font-thin text-gray-500 uppercase">
+                    ,
+                </span>
+            )}
+        </div>
+    ));
+};
+
 const ChangelogItem = ({ changelog, index }) => {
     const borderTopClass = index === 0 ? "" : "border-t border-gray-200";
+    const coAuthors = parseCoAuthors(changelog.message);
+
     return (
         <div className={`flex items-center ${borderTopClass} h-16`}>
             <div className="w-1/4 text-xxs font-thin text-gray-500 sm:w-2/12">
@@ -22,11 +53,12 @@ const ChangelogItem = ({ changelog, index }) => {
             <div className="w-1/4 text-xs font-thin uppercase sm:w-2/12">
                 {changelog.projectName}
             </div>
-            <div className="w-1/4 text-xs font-thin text-gray-500 uppercase sm:w-2/12">
+            <div className="w-1/4 text-xs font-thin text-gray-500 uppercase sm:w-2/12 flex items-center">
                 By{" "}
-                <span className="text-xxs bg-black text-white">
-                    {changelog.by}
-                </span>
+
+
+
+                {displayAuthors([changelog.by].concat(coAuthors))}
             </div>
             <div className="w-1/4 text-xxs font-thin text-gray-500 uppercase sm:w-2/12">
                 @{" "}
@@ -34,9 +66,8 @@ const ChangelogItem = ({ changelog, index }) => {
                     {parseDate(changelog.dateTime)}
                 </span>
             </div>
-            {/* On small screens, the message will be hidden to maintain the layout */}
             <div className="hidden w-1/4 text-xs font-thin uppercase overflow-hidden sm:w-4/12 sm:block">
-                {changelog.message}
+                {parseMessage(changelog.message)}
             </div>
         </div>
     );
